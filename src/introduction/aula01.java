846318b1dbdb1d2a6f9abf62ca9249ab38e29651
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-08-01 18:23:24 -0300

    aula01

---
+++ src/introduction/aula01.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class aula01 {
+    //simple comment
+    /*
+    comment on two or more lines
+     */
+
+    /***
+     *
+     * professional comment
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
