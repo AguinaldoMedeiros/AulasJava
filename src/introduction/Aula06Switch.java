2ed0144b17b1784b6e2bf326c5dfa290b93d9305
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-08-03 13:03:34 -0300

    Aula06Switch

---
+++ src/introduction/Aula06Switch.java
@@ -0,0 +1,36 @@
+package introduction;
+
+public class Aula06Switch {
+
+    public static void main(String[] args) {
+
+        int age = 11;
+
+        switch (age){
+
+            case 1:
+                System.out.println("None");
+                break;
+            case 2:
+                System.out.println("None");
+                break;
+            case 3:
+                System.out.println("None");
+                break;
+            case 4:
+                System.out.println("None");
+                break;
+            case 7:
+                System.out.println("None");
+                break;
+            case 9:
+                System.out.println("None");
+                break;
+            case 10:
+                System.out.println("Match!");
+                break;
+            default:
+                System.out.println("Not match... :(");
+        }
+    }
+}
