d02fb6c0508d96ea641d9ac610dd04a6346d4367
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-08-03 12:40:37 -0300

    Aula03Operators

---
+++ src/introduction/Aula03Operators.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class Aula03Operators {
+
+    public static void main(String[] args) {
+        // + - / *
+        // %
+        // < > <= >= == !=
+        // && || !
+        // = += -= *= /= %=
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num2-num1);
+    }
+}
