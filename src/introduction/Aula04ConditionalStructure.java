25dde554227d4f384bd5bdff3efb765d6bdba1b1
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-08-03 12:50:17 -0300

    Aula03ConditionalStructure

---
+++ src/introduction/Aula04ConditionalStructure.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class Aula04ConditionalStructure {
+
+    public static void main(String[] args) {
+
+        int age = 9;
+        if (age >= 10){
+            System.out.println("Your age are: "+age);
+        } else if(age == 9){
+            System.out.println("Your age are 9 ");
+        } else{
+            System.out.println("None");
+        }
+    }
+}
